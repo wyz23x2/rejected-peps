16ab3a7421cb5a679a1bc6f857c5cac5bb4af2bc
wyz23x2 <52805709+wyz23x2@users.noreply.github.com>
2021-08-08 14:37:13 +0800

    Final update

--- setup.py
+++ setup.py
@@ -11,16 +11,20 @@ setuptools.setup(
     description="A python package that implements some rejected or withdrawn PEPs.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://gitee.com/wyz23x2/rejected/",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://gitee.com/wyz23x2/rejected-peps/issues",
+        "Wiki": "https://gitee.com/wyz23x2/rejected-peps/wikis"
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Development Status :: 3 - Alpha",
     ],
-    package_dir={"": "src"},
+    package_dir={"": "rejected-peps"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
 )
