6ebe758ea988a44c9507251f0043a9caf31deaf8
wyz23x2 <52805709+wyz23x2@users.noreply.github.com>
2021-08-08 19:53:23 +0800

    Add PEP 204

---
+++ rejected_peps/pep204.py
@@ -0,0 +1,37 @@
+import sys
+import textwrap
+class pep204:
+    """\
+PEP INFO
+
+PEP 204 -- Range Literals
+Status: Rejected
+Created: 2000-07-14
+
+MODULE INFO
+
+This module supports indexing for range literals:
+>>> import rejected_peps.pep204 as r
+>>> r[5]
+range(0, 5)
+>>> r[2:5]
+range(2, 5)
+>>> r[60:40:-2]
+range(60, 40, -2)
+
+REFERENCES
+
+PEP 204: <https://www.python.org/dev/peps/pep-204/>
+"""
+    def __getitem__(self, x):
+        if isinstance(x, slice):
+            start, stop, step = x.start, x.stop, x.step
+            if stop is None:
+                raise ValueError('Stop is required')
+            if start is None:
+                start = 0
+            if step is None:
+                step = 1
+            return range(start, stop, step)
+        return range(x)
+sys.modules[__name__] = pep204()
