f5896e9c5859bed3f9be976ca3064e7d96aa0d0a
wyz23x2 <52805709+wyz23x2@users.noreply.github.com>
2021-08-21 15:11:40 +0800

    Improve PEP 294

--- rejected_peps/pep294.py
+++ rejected_peps/pep294.py
@@ -7,19 +7,44 @@ Created: 2002-06-19
 
 MODULE INFO
 
-This module adds the lowercase regular version as in the PEP to 
-the types module and this module. If the new name is invalid 
-(e.g. `lambda`), a trailing underscore is added.
+The apply(module=types, *, rename=underscore, strict=bool) function
+adds the lowercase regular version as in the PEP to 
+the module. If the new name is invalid (e.g. `lambda`), rename(name)
+is called. If strict is True, then the new return value of rename()
+and the type (should be str) will be checked. This is done in-place
+so apply() returns None.
+The underscore(), title() and original() functions are provided
+for the rename parameter.
+See the docstrings for more information.
 
 REFERENCES
 
 PEP 294: <https://www.python.org/dev/peps/pep-0294/>
 """
-import types
-for name in dir(types):
-    if name[-4:] == 'Type' and name[:-4]:
-        new_name = name[:-4].lower()
-        if not new_name.isidentifier():
-            new_name = f'{new_name}_'
-        globals()[new_name] = getattr(types, name)
-        setattr(types, new_name, getattr(types, name))
+import keyword as _k
+def underscore(s: str) -> str:
+    """Appends an underscore (_) to s."""
+    return f'{s}_'
+title = str.title
+def original(s: str) -> str:
+    """Returns s."""
+    return s
+def valid(name: str) -> bool:
+    """True if name is an identifier & not a keyword. Note that non-str types return True."""
+    if not isinstance(name, str):
+        return True
+    return _k.iskeyword(name) + (not name.isidentifier()) == 0
+def apply(module=None, *, rename=underscore, strict: bool = True):
+    types = module or __import__('types')
+    for name in dir(types):
+        if name[-4:] == 'Type' and name[:-4]:
+            new_name = name[:-4].lower()
+            if not valid(new_name):
+                r = rename(new_name)
+                if strict and not valid(r):
+                    raise ValueError(f'Invalid name {r!r}')
+                new_name = r
+            if strict and type(r) is not str:
+                raise TypeError(f'Invalid name {new_name!r} with type '
+                                f'{type(r).__name__!r}')
+            setattr(types, new_name, getattr(types, name))
