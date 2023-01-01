f5170797d10dd312c1e3ee70779259f79e2f1bfd
wyz23x2 <wyz23x2@163.com>
2023-01-01 11:58:09 +0800

    Bump to 0.9.0b1

--- rejected_peps/__init__.py
+++ rejected_peps/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.9.0a2'
+__version__ = '0.9.0b1'
 import importlib as _imp
 from collections import namedtuple as _nt
 from itertools import chain as _chain
@@ -91,6 +91,7 @@ def _search_one(*s, strict: bool = True) -> _O[int]:
         raise ValueError(f'No match found')
     return xs[0]
 search.one = _search_one
+_w.simplefilter('error', DeprecationWarning)
 def search_one(*args, **kwargs):
     _w.warn('search_one() is deprecated and will be removed in v0.9.',
             DeprecationWarning, 2)
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
         "Typing :: Typed",
     ],
