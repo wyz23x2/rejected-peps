532320bd9e378e77c52ebc784da5dff5f9294fe2
wyz23x2 <wyz23x2@163.com>
2023-01-05 09:26:52 +0800

    [doc] Format pep754.py

--- rejected_peps/pep754.py
+++ rejected_peps/pep754.py
@@ -21,12 +21,17 @@ NaN = float('nan')
 PosInf = float('inf')
 NegInf = -PosInf
 def isNaN(value) -> bool:
+    """Return True if x is a NaN (not a number), and False otherwise."""
     return _math.isnan(value)
 def isPosInf(value) -> bool:
+    """Return True if x is a positive infinity, and False otherwise."""
     return _math.isinf(value) and value > 0
 def isNegInf(value) -> bool:
+    """Return True if x is a negative infinity, and False otherwise."""
     return _math.isinf(value) and value < 0
 def isFinite(value) -> bool:
+    """Return True if x is neither an infinity nor a NaN, and False otherwise."""
     return _math.isfinite(value)
 def isInf(value) -> bool:
+    """Return True if x is a positive or negative infinity, and False otherwise."""
     return _math.isinf(value)
